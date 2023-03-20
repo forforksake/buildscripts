45ae0e61184c4a72a7a07f6bfa58f89f0d783699
For Fork Sake <sakeforfork@gmail.com>
2023-03-20 03:45:04 +0000

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -36,19 +36,6 @@ sudo install repo /usr/local/bin/repo
 rm repo
 echo -e "Done."
 
-echo -e "\nInstalling Android SDK platform tools..."
-wget -q https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-unzip -qq platform-tools-latest-linux.zip
-rm platform-tools-latest-linux.zip
-echo -e "Done."
-
-echo -e "\nInstalling Google Drive CLI..."
-wget -q https://raw.githubusercontent.com/usmanmughalji/gdriveupload/master/gdrive
-chmod a+x gdrive
-sudo install gdrive /usr/local/bin/gdrive
-rm gdrive
-echo -e "Done."
-
 echo -e "\nInstalling apktool and JADX..."
 mkdir -p bin
 wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.6.0.jar -O bin/apktool.jar
@@ -75,101 +62,6 @@ repofastsync() { time schedtool -B -e ionice -n 0 `which repo` sync -c --force-s
 # List lib dependencies of any lib/bin
 list_blob_deps() { readelf -d $1 | grep "\(NEEDED\)" | sed -r "s/.*\[(.*)\]/\1/"; }
 
-export TZ='Asia/Kolkata'
-export USE_CCACHE=1
-export CCACHE_EXEC=/usr/bin/ccache
-
-alias gup="gdrive upload --share"
-
-function msg() {
-  echo -e "\e[1;32m$1\e[0m"
-}
-
-function helptree() {
-  if [[ -z $1 && -z $2 ]]; then
-    msg "Usage: helptree <tag> <add/pull>"
-    return
-  fi
-  kernel_version="$( cat Makefile | grep VERSION | head -n 1 | sed "s|.*=||1" | sed "s| ||g" )"
-  kernel_patchlevel="$( cat Makefile | grep PATCHLEVEL | head -n 1 | sed "s|.*=||1" | sed "s| ||g" )"
-  version=$kernel_version.$kernel_patchlevel
-  if [[ $version != "4.14" && $version != "5.4" ]]; then
-    msg "Kernel $version not supported! Only msm-5.4 and msm-4.14 are supported as of now."
-    return
-  fi
-  if [[ -z $3 ]]; then
-    spec=all
-  else
-    spec=$3
-  fi
-  if [[ $2 = "add" ]]; then
-    tree_status="Adding"
-    commit_status="Import from"
-  else
-    tree_status="Updating"
-    commit_status="Merge"
-    if [[ $spec = "all" ]]; then
-      msg "Merging kernel as of $1.."
-      git fetch https://git.codelinaro.org/clo/la/kernel/msm-$version $1 &&
-      git merge FETCH_HEAD -m "Merge tag '$1' of msm-$version"
-    fi
-  fi
-  if [[ $spec = "wifi" || $spec = "all" ]]; then
-    for i in qcacld-3.0 qca-wifi-host-cmn fw-api; do
-      msg "$tree_status $i subtree as of $1..."
-      git subtree $2 -P drivers/staging/$i -m "$i: $commit_status tag '$1'" \
-        https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/$i $1
-    done
-  fi
-  if [[ $spec = "techpack" || $spec = "all" ]]; then
-    msg "$tree_status audio-kernel subtree as of $1..."
-    git subtree $2 -P techpack/audio -m "techpack: audio: $commit_status tag '$1'" \
-      https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel $1
-    if [[ $version = "5.4" ]]; then
-      msg "$tree_status camera-kernel subtree as of $1..."
-      git subtree $2 -P techpack/camera -m "techpack: camera: $commit_status tag '$1'" \
-        https://git.codelinaro.org/clo/la/platform/vendor/opensource/camera-kernel $1
-      msg "$tree_status dataipa subtree as of $1..."
-      git subtree $2 -P techpack/dataipa -m "techpack: dataipa: $commit_status tag '$1'" \
-        https://git.codelinaro.org/clo/la/platform/vendor/opensource/dataipa $1
-      msg "$tree_status datarmnet subtree as of $1..."
-      git subtree $2 -P techpack/datarmnet -m "techpack: datarmnet: $commit_status tag '$1'" \
-        https://git.codelinaro.org/clo/la/platform/vendor/qcom/opensource/datarmnet $1
-      msg "$tree_status datarmnet-ext subtree as of $1..."
-      git subtree $2 -P techpack/datarmnet-ext -m "techpack: datarmnet-ext: $commit_status tag '$1'" \
-        https://git.codelinaro.org/clo/la/platform/vendor/qcom/opensource/datarmnet-ext $1
-      msg "$tree_status display-drivers subtree as of $1..."
-      git subtree $2 -P techpack/display -m "techpack: display: $commit_status tag '$1'" \
-        https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers $1
-      msg "$tree_status video-driver subtree as of $1..."
-      git subtree $2 -P techpack/video -m "techpack: video: $commit_status tag '$1'" \
-        https://git.codelinaro.org/clo/la/platform/vendor/opensource/video-driver $1
-    elif [[ $version = "4.14" ]]; then
-      if [[ $2 = "add" ]] || [ -d "techpack/data" ]; then
-        msg "$tree_status data-kernel as of $1..."
-        git subtree $2 -P techpack/data -m "techpack: data: $commit_status tag '$1'"  \
-          https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/data-kernel $1
-      fi
-    fi
-  fi
-}
-
-function addtree() {
-  if [[ -z $1 ]]; then
-    msg "Usage: addtree <tag> [optional: spec]"
-    return
-  fi
-  helptree $1 add $2
-}
-
-function updatetree() {
-  if [[ -z $1 ]]; then
-    msg "Usage: updatetree <tag> [optional: spec]"
-    return
-  fi
-  helptree $1 pull $2
-}
-EOF
 
 # Add android sdk to path
 cat <<'EOF' >> .profile
@@ -188,9 +80,6 @@ echo -e "Done."
 
 # Increase tmux scrollback buffer size
 echo "set-option -g history-limit 6000" >> .tmux.conf
-
-# Increase maximum ccache size
-ccache -M 100G
 
 ###
 ### IMPORTANT !!! REPLACE WITH YOUR PERSONAL DETAILS IF NECESSARY
